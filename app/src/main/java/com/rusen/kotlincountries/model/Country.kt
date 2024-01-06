adaa9d03df4e001453369a034be9598bd7d77ee2
rusenolcay <narincrusen@gmail.com>
2024-01-06 23:50:02 +0300

    init

--- app/src/main/java/com/rusen/kotlincountries/model/Country.kt
+++ app/src/main/java/com/rusen/kotlincountries/model/Country.kt
@@ -1,25 +1,38 @@
 package com.rusen.kotlincountries.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Country(
 
+    @ColumnInfo("name")
     @SerializedName("name")
     val countryName: String?,
 
+    @ColumnInfo("capital")
     @SerializedName("capital")
     val countryCapital: String?,
 
+    @ColumnInfo("region")
     @SerializedName("region")
     val countryRegion: String?,
 
+    @ColumnInfo("currency")
     @SerializedName("currency")
     val countryCurrency: String?,
 
+    @ColumnInfo("language")
     @SerializedName("language")
     val countryLanguage: String?,
 
+    @ColumnInfo("flag")
     @SerializedName("flag")
-    val imageUrl: String?
-)
+    val imageUrl: String?) {
+
+    @PrimaryKey
+    val uuid: Int = 0
+}
 
